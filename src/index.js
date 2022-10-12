fd5d94085715d1411fc813883c44344c7e9141e4
PameSegovia <114256835+PameSegovia@users.noreply.github.com>
2022-10-12 01:30:33 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import validator from './validator.js';
 
-console.log(validator);
+//SELECCIONAR MES
+for (let i = 1; i <= 12; i++) {
+  let option = document.createElement('option');
+  option.value = i;
+  option.innerText = i;
+  document.getElementById("selectMes").appendChild(option);
+}
+
+//SELECCIONAR AÑO
+const year = new Date().getFullYear();
+for (let i = year; i <= year + 8; i++) {
+  let option = document.createElement('option');
+  option.value = i;
+  option.innerText = i;
+  document.getElementById("selectYear").appendChild(option);
+}
+
+//Evento botón validar tarjeta
+const btnValidar = document.getElementById("btnValidar");
+btnValidar.addEventListener("click", llamar);
+
+function llamar() {
+  const numero = document.getElementById("numTarjeta");
+  let creditCardNumber = numero.value;
+  let result = validator.isValid(creditCardNumber);
+  let respuesta = validator.maskify(creditCardNumber)
+  if (result == true) {
+    alert("Su Tarjeta N° " + (respuesta) + " es válida");
+  }
+  else {
+    alert("Tarjeta Inválida");
+  }
+}
